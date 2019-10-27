e9e010aadb985a5447558ee48d242740a68b05c1
Matt Kilcup <nevermindthelabel@gmail.com>
2019-10-26 17:44:40 -0700

    added project files

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<div id="footer" class="cf">
+    <div class="column three">
+        <strong>Phone</strong>
+        123-456-7890
+    </div>
+    <div class="column three">
+        <strong>Address</strong>
+        123 Fake Street
+    </div>
+    <div class="column three last">
+        <strong>Hours</strong>
+        <i>Tuesday - Thursday</i><br />
+        1:00pm - 9:00pm
+        <i>Friday - Saturday</i><br />
+        4:00pm - 11:00pm
+        <i>Sunday - Monday</i><br />
+        Closed
+    </div>
+</div>
+<small>&copy; <?php echo date('Y') ?>
+    </div>
+    </div>
+    </div>
+    </div>
+
+    </body>
+
+    </html>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title></title>
+    <link href="assets/styles.css" rel="stylesheet">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+
+<body id="final-example">
+    <div class="wrapper">
+        <div id="banner">
+            <a href="/"><img src="img/banner.png" alt="fine dining"></a>
+            <div id="nav">
+                    <?php include('includes/nav.php'); ?>
+                <div class="content">
